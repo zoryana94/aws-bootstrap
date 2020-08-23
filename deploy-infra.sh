9553e7b0a19b9482b94c5da87ccc136b2c3efcf9
Zoryana Vakh <zoryana@leanconvert.com>
2020-08-23 13:23:23 +0300

    Add deployment

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -23,6 +23,7 @@ aws cloudformation deploy \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
   CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
@@ -33,7 +34,12 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-  EC2InstanceType=$EC2_INSTANCE_TYPE
+  EC2InstanceType=$EC2_INSTANCE_TYPE \
+  GitHubOwner=$GH_OWNER \
+  GitHubRepo=$GH_REPO \
+  GitHubBranch=$GH_BRANCH \
+  GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+  CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
--- stop-service.sh
+++ stop-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
-source /home/ec2-user/.bash_profile
-[ -d "/home/ec2-user/app/release" ] && \
-cd /home/ec2-user/app/release && \
+source /home/root/.bash_profile
+[ -d "/home/root/app/release" ] && \
+cd /home/root/app/release && \
 npm stop
